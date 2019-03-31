b5ba9df37b09ca5ef4bc3f238189062dd40f31b1
zhangzhe <peeryuan@gmail.com>
2019-03-31 09:29:05 +0800

    excise

--- src/clss.ts
+++ src/clss.ts
@@ -1,3 +1,5 @@
+import "reflect-metadata"
+
 class Person{
   
   constructor(public name: String) {
@@ -54,3 +56,50 @@ enum Color {
 
 let myColor = Color.Blue;
 console.log(myColor);
+
+let obj1 = {
+  toString() {
+    console.log('toString called');
+    return 'Hello';
+  }
+}
+
+const foo = 'hello world';
+console.log(typeof foo);
+
+@Reflect.metadata('inClass', 'A')
+class Test {
+  @Reflect.metadata('inMethod', 'B')
+  public hello(): String {
+    return 'hello world';
+  }
+}
+
+console.log(Reflect.getMetadata('inClass', Test));
+console.log(Reflect.getMetadata('inMethod', new Test(), 'hello'));
+
+function classDecorator(): ClassDecorator {
+  return (target) => {
+    Reflect.defineMetadata('classMetaData', 'a', target);
+  }
+}
+
+function methodDecorator(): MethodDecorator {
+  return (target, property, descriptor) => {
+    Reflect.defineMetadata('methodMetaData', 'b', target, property);
+  }
+}
+
+@classDecorator()
+class SomeClass {
+  @methodDecorator()
+  someMethod() {}
+}
+
+const scClassData = Reflect.getMetadata('classMetaData', SomeClass);
+const scMethodData = Reflect.getMetadata('methodMetaData', new SomeClass(), 'someMethod');
+
+console.log(`class data ${scClassData}`);
+console.log(`method data ${scMethodData}`);
+
+
--- src/forof.ts
+++ src/forof.ts
@@ -5,3 +5,109 @@ for (const [index, color] of colors.entries()) {
   console.log(`index: ${index}, color: ${color}`);
 }
 */
+
+const map = (arr: any[], fn: any) => [].map.call(arr, fn);
+
+const arr = [1, 3, 4];
+const [_, ...rest] = arr;
+
+function thousandComma1(num: string) {
+  const numList = num.split('').reverse();
+
+  let count = 0;
+  const reCommaList = numList.reduce((acc: string[], cur: string) => {
+    if (count > 0 && count % 3 == 0) {
+      acc.push(',');
+    }
+    count++;
+    acc.push(cur);
+    return acc;
+  }, []);
+
+  return reCommaList.reverse().join('');
+}
+
+function thousandComma(num: String): string {
+  const numList = num.split('').reverse();
+
+  const res: string[] = [];
+  numList.forEach((n, index) => {
+    if (index > 0 && index % 3 === 0) {
+      res.push(',');
+    }
+    res.push(n);
+  });
+
+  return res.reverse().join('');
+}
+
+console.log(thousandComma('100'));
+console.log(thousandComma('1001'));
+console.log(thousandComma('10011'));
+console.log(thousandComma('100111'));
+console.log(thousandComma('1001111'));
+console.log(thousandComma('10011111'));
+
+function groupBy(objList: any[], propFn: Function) {
+  return objList.reduce((acc, o) => {
+    const key = propFn(o);
+    const vList = acc[key] || [];
+    vList.push(o);
+    acc[key] = vList;
+    return acc;
+  }, {});
+}
+
+const albums = [
+  {title: 'Sabbath Bloody Sabbath', gen: 'Metal'},
+  {title: 'Scientist', gen: 'Dub'},
+  {title: '', gen: 'Metal'},
+];
+
+console.log(JSON.stringify(groupBy(albums, (o: any) => o.gen)));
+
+function toPairList(obj: any) {
+  return Object.keys(obj).map((k) => {
+    return [k, obj[k]];
+  });
+}
+
+function pairsToObj(pairList: any[]) {
+  return pairList.reduce((o, pair) => {
+    const [k, v] = pair;
+    o[k] = v;
+    return o;
+  }, {});
+}
+
+const obj = { name: 'hello', title: 'world' };
+console.log(JSON.stringify(toPairList(obj)));
+console.log(JSON.stringify(pairsToObj(toPairList(obj))));
+
+
+const condition = true;
+const objExt = {
+  ...condition && obj,
+};
+
+console.log(JSON.stringify(objExt));
+
+function plunk(o, prop = '') {
+  const propList = prop.split('.').filter(o => !!o);
+
+  return propList.reduce((acc, cur) => {
+    return acc && acc[cur];
+  }, o);
+}
+
+const o = {
+  name: {
+    first: 'aaa',
+    last: 'bbb',
+  },
+};
+
+console.log(JSON.stringify(plunk(o)));
+console.log(JSON.stringify(plunk(o, 'name')));
+console.log(JSON.stringify(plunk(o, 'name.first')));
+console.log(JSON.stringify(plunk(o, 'name.fir')));
